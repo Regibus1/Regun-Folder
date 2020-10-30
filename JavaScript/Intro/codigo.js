dc4907e85f5685394d5b9893cea7aff2aadbc60c
Regibus <abdiasejr@gmail.com>
2020-10-30 20:49:06 -0300

    Version Octubre

---
+++ JavaScript/Intro/codigo.js
@@ -0,0 +1,17 @@
+/* tipos de datos */
+string = 'cadena de texto';
+number = 19;
+boolean = false;
+
+/*tipos de variables */
+var nombre = null, apellido = null;
+let numero = 13;
+const IMPUESTO = 0.31;
+
+/* Funcion Prompt */
+
+let respuesta = prompt("Hola internauta, dime tu nombre"); 
+
+/* Alert */
+
+alert(`Hola ${respuesta}, como andás?`);
